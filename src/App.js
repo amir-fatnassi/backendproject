6c8efacc950714a712fbce1f6e06405463c01b84
amir <fatnasiamir@yahoo.fr>
2019-10-06 23:22:32 +0200

    backendproject

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/navbar.component';
+import ExercisesList from './components/exercises-list.component';
+import EditExercises from './components/edit-exercises.component';
+import CreateExercise from './components/create-exercise.component';
+import CreateUser from './components/create-user.component';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="container">
+      <Route exact path="/" component={ExercisesList} />
+      <Route exact path="/edit/:id" component={EditExercises} />
+      <Route  path="/create" component={CreateExercise} />
+      <Route  path="/user" component={CreateUser} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
